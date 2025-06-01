8e91715099d57b36d2447435a79feb69246de44e
Ester Costa de Souza <alwesterc@gmail.com>
2025-06-01 19:17:47 -0300

    feat(list-rooms): melhorias e ajustes no componente de listagem de salas

--- src/components/ListRooms.jsx
+++ src/components/ListRooms.jsx
@@ -2,25 +2,21 @@ import { Row, Col } from 'react-bootstrap';
 
 import { Card } from './Card';
 
-export function ListRooms({ rooms, navigate }) {
+export function ListRooms({ rooms, goToRoom, editRoom, deleteRoom }) {
 	return (
-		<Row>
+		<Row aria-label="Lista de salas de jogos">
 			{rooms.map((room, idx) => (
-				<Col key={idx} md={4} className="mb-4">
+				<Col key={idx} md={4} sm={6} xs={12} className="mb-4">
 					<Card
 						image={room.imagem}
 						title={room.nome}
-						subtitle={room.subtitle}
+						nameRoom={room.nameRoom}
 						description={room.description}
 						data={room.data}
-						players={room.players}
-						onClick={() => {
-							navigate(`/players/${room.id}`, {
-								state: {
-									room
-								}
-							});
-						}}
+						players={room.playersList.length || 0}
+						editRoom={() => editRoom(room)}
+						deleteRoom={() => deleteRoom(room)}
+						onClick={() => goToRoom(room)}
 					/>
 				</Col>
 			))}
