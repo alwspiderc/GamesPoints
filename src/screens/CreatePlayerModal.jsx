104827f7efd8bedf27b4a9a0fda60de78790e187
Ester Costa de Souza <alwesterc@gmail.com>
2025-06-01 19:18:09 -0300

    feat(player-modal): melhorias e correções no modal de criação de jogador

--- src/screens/CreatePlayerModal.jsx
+++ src/screens/CreatePlayerModal.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Form, Row, Col, Alert } from 'react-bootstrap';
 import { Button } from '../components/Button';
+import { useLocation } from 'react-router-dom';
 
 export function CreatePlayerModal({ show, onHide, onCreate }) {
 	const [form, setForm] = useState({
@@ -10,6 +11,7 @@ export function CreatePlayerModal({ show, onHide, onCreate }) {
 	});
 	const [error, setError] = useState('');
 	const [success, setSuccess] = useState(false);
+	const { room } = useLocation().state;
 
 	const handleChange = (e) => {
 		const { name, value, files } = e.target;
@@ -27,16 +29,62 @@ export function CreatePlayerModal({ show, onHide, onCreate }) {
 		}
 		setError('');
 		setSuccess(true);
+
+		const randomPlayerImages = [
+			'https://images.pexels.com/photos/1181696/pexels-photo-1181696.jpeg',
+			'https://images.pexels.com/photos/907156/pexels-photo-907156.jpeg',
+			'https://images.pexels.com/photos/1130626/pexels-photo-1130626.jpeg',
+			'https://images.pexels.com/photos/1707828/pexels-photo-1707828.jpeg',
+			'https://images.pexels.com/photos/3771069/pexels-photo-3771069.jpeg',
+			'https://images.pexels.com/photos/163077/mario-arcade-game-video-163077.jpeg',
+			'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg',
+			'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg',
+			'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg',
+			'https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg',
+			'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg',
+			'https://images.pexels.com/photos/1382731/pexels-photo-1382731.jpeg',
+			'https://images.pexels.com/photos/1043474/pexels-photo-1043474.jpeg',
+			'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg',
+			'https://images.pexels.com/photos/774909/pexels-photo-774909.jpeg',
+			'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg',
+			'https://images.pexels.com/photos/1689731/pexels-photo-1689731.jpeg',
+			'https://images.pexels.com/photos/1587009/pexels-photo-1587009.jpeg',
+			'https://images.pexels.com/photos/1462980/pexels-photo-1462980.jpeg'
+		];
+		const randomImage =
+			randomPlayerImages[Math.floor(Math.random() * randomPlayerImages.length)];
+
+		const newPlayer = {
+			name: form.name,
+			description: form.description,
+			image: randomImage,
+			score: 0
+		};
+
+		fetch(`http://localhost:3001/rooms/${room.id}`, {
+			method: 'PATCH',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ playersList: [...room.playersList, newPlayer] })
+		});
+		setSuccess(true);
 		setTimeout(() => {
 			setSuccess(false);
-			onCreate(form);
+			onCreate();
 		}, 1000);
 	};
+
+	useEffect(() => {
+		setForm({
+			name: '',
+			description: '',
+			image: null
+		});
+	}, [show]);
 
 	return (
 		<Modal show={show} onHide={onHide} centered style={styles.modal} size="xl">
 			<Modal.Header>
-				<Modal.Title>Crie seu jogador</Modal.Title>
+				<Modal.Title>Crie jogador</Modal.Title>
 				<i
 					className="bi bi-x-lg"
 					onClick={onHide}
