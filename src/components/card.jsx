77884f8ac9855b5f08f9b5f67f76c7a1257f3214
Ester Costa de Souza <alwesterc@gmail.com>
2025-06-01 19:17:53 -0300

    refactor(card): melhorias e refatoração no componente card

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,14 +1,29 @@
 import { Card as CardBootstrap } from 'react-bootstrap';
+import { Button } from './Button';
 
 export function Card(props) {
-	const { image, title, subtitle, description, data, players, onClick } = props;
+	const {
+		image,
+		title,
+		nameRoom,
+		description,
+		data,
+		players,
+		onClick,
+		editRoom,
+		deleteRoom
+	} = props;
 	return (
-		<CardBootstrap style={styles.card} onClick={onClick}>
+		<CardBootstrap
+			style={styles.card}
+			onClick={onClick}
+			aria-label={`Sala de jogo: ${title}`}
+		>
 			<CardBootstrap.Img variant="top" src={image} style={styles.image} />
 			<CardBootstrap.Body style={styles.cardBody}>
 				<CardBootstrap.Title style={styles.title}>{title}</CardBootstrap.Title>
-				<CardBootstrap.Text style={styles.subtitle}>
-					{subtitle}
+				<CardBootstrap.Text style={styles.nameRoom}>
+					{nameRoom}
 				</CardBootstrap.Text>
 				<CardBootstrap.Text style={styles.description}>
 					{description}
@@ -17,6 +32,25 @@ export function Card(props) {
 				<CardBootstrap.Text style={styles.players}>
 					{players}
 				</CardBootstrap.Text>
+
+				<Button
+					onClick={(e) => {
+						e.stopPropagation();
+						editRoom();
+					}}
+					ariaLabel="Editar Sala"
+				>
+					Editar Sala
+				</Button>
+				<Button
+					onClick={(e) => {
+						e.stopPropagation();
+						deleteRoom();
+					}}
+					ariaLabel="Deletar Sala"
+				>
+					Deletar Sala
+				</Button>
 			</CardBootstrap.Body>
 		</CardBootstrap>
 	);
@@ -27,15 +61,17 @@ const styles = {
 		border: 'none',
 		borderRadius: 0,
 		overflow: 'hidden',
-		cursor: 'pointer'
+		cursor: 'pointer',
+		marginBottom: 16,
+		width: '100%'
 	},
 	image: {
 		height: 200,
-		objectFit: 'cover'
+		objectFit: 'cover',
+		width: '100%'
 	},
 	cardBody: {
 		background: '#e0e0e0',
-
 		display: 'flex',
 		flexDirection: 'column',
 		gap: 4
@@ -44,7 +80,7 @@ const styles = {
 		fontWeight: 700,
 		marginBottom: 0
 	},
-	subtitle: {
+	nameRoom: {
 		color: '#FF6767',
 		fontWeight: 500,
 		fontSize: 16,
