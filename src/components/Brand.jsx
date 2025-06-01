9e1852505a3c6c5f1eb91be621e62a6dafa5bec9
Ester Costa de Souza <alwesterc@gmail.com>
2025-06-01 19:17:37 -0300

    style(brand): ajustes visuais no componente Brand

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -2,7 +2,11 @@ import { Navbar as NavbarBootstrap } from 'react-bootstrap';
 
 export function Brand() {
 	return (
-		<NavbarBootstrap.Brand href="#" style={styles.brand}>
+		<NavbarBootstrap.Brand
+			href="#"
+			style={styles.brand}
+			aria-label="GamesPoints - Página inicial"
+		>
 			<span style={{ color: '#FF6767' }}>Games</span>
 			<span style={{ color: '#222' }}>Points</span>
 		</NavbarBootstrap.Brand>
