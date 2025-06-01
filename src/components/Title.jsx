f4c931aeee4d7b556aef166cf975462781f525d4
Ester Costa de Souza <alwesterc@gmail.com>
2025-06-01 19:17:50 -0300

    style(title): ajustes de estilo no componente Title

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,5 +1,9 @@
-export function Title({ children }) {
-	return <h3 style={styles}>{children}</h3>;
+export function Title({ children, style }) {
+	return (
+		<h3 style={{ styles, ...style }} role="heading" tabIndex={0}>
+			{children}
+		</h3>
+	);
 }
 
 const styles = {
